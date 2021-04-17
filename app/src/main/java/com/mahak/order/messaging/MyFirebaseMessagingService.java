cbdd750dfdf7db6b4967b8f4ddeafffe2f0818e8
saeid mohammadi <saeid.mhd@gmail.com>
2021-04-17 17:17:46 +0430

    add firebase cloud messaging

---
+++ app/src/main/java/com/mahak/order/messaging/MyFirebaseMessagingService.java
@@ -0,0 +1,12 @@
+package com.mahak.order.messaging;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+    }
+}
