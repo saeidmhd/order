21225fbe53bca516ec57fe6db68aa2daaff66b82
saeid mohammadi <saeid.mhd@gmail.com>
2021-07-07 12:18:26 +0430

    fix --->product count after filter and change asset and product group

--- app/src/main/java/com/mahak/order/storage/DbAdapter.java
+++ app/src/main/java/com/mahak/order/storage/DbAdapter.java
@@ -4796,7 +4796,7 @@ public class DbAdapter {
             cursor = mDb.rawQuery(" select count(*) from Products inner join ProductDetail on Products.productId = ProductDetail.productId and Products.UserId = ProductDetail.UserId " +
                     " where ( " + LikeStr + " or " + DbSchema.Productschema.TABLE_NAME + "." + DbSchema.Productschema.COLUMN_PRODUCT_CODE + " LIKE " + "'%" + searchStr + "%'"  + " ) and " + DbSchema.Productschema.TABLE_NAME + "." + DbSchema.Productschema.COLUMN_Deleted + " = " + " 0 " +
                     " and " + DbSchema.Productschema.TABLE_NAME + "." + DbSchema.Productschema.COLUMN_USER_ID + " = " + getPrefUserId() +
-                    getProductCategoryStrnig(CategoryId) + getProductAssetStrnig(MODE_ASSET) + " GROUP by Products.productId " +
+                    getProductCategoryStrnig(CategoryId) + getProductAssetStrnig(MODE_ASSET) +
                     " order by " + orderBy, null);
             if (cursor != null) {
                 cursor.moveToFirst();
